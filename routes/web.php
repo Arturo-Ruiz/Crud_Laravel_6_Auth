01afffff64848d5ced82890f0933c61c7ce49499
Gregory <gregory09ruiz@gmail.com>
2020-07-07 14:18:55 -0400

    resolved_Namespace

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::resource('Products', 'ProductController');
+Route::resource('products', 'ProductController');
