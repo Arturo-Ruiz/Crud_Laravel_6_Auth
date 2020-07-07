f3ba929b4d56b1a2f395615c8fb686a23b903780
Gregory <gregory09ruiz@gmail.com>
2020-07-07 15:58:35 -0400

    include_info

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,6 +15,7 @@
             <h2>Listado de Productos
                 <a href="{{ route('products.create') }}" class="btn btn-primary pull-right">Nuevo</a>
             </h2>
+            @include('products.fragment.info')
             <table class="table table-hover table-striped">
                 <thead>
                     <tr>
