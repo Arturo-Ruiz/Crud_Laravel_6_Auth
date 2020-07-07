380447dca69cc1afc874e6cd20e9a744ff71324e
Gregory <gregory09ruiz@gmail.com>
2020-07-07 16:22:26 -0400

    Created_view_create

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+
+    @if (Auth::check())
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="page-header text-center">CRUD Laravel 6 con Autenticación</h1>
+        </div>
+            <br>
+            <hr>
+            <hr>
+            <br>
+        <div class="col-md-8">
+            <h2>Nuevo Producto
+                <a class="btn btn-primary pull-right" href="{{ route('products.index')}}">Listado</a>
+            </h2>
+
+
+        </div>
+        <div class="col-md-4">
+            @include('products.fragment.aside')
+        </div>
+
+    </div>
+    @else
+    <h1 class="page-header text-center">Debe iniciar sesion para continuar</h1>
+
+    @endif
+</div>
+@endsection
+
