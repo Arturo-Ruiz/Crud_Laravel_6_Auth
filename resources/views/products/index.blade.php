8762363a16ad7781ae04507f4e28ea9e07b41296
Gregory <gregory09ruiz@gmail.com>
2020-07-07 14:59:19 -0400

    View-Index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,19 @@
 @extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="page-header text-center">CRUD Laravel 6 con Autenticación</h1>
+        </div>
+
+        <div class="col-md-8">
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+
+    </div>
+</div>
+@endsection
+
