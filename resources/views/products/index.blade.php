92a8d33400f41b0066e35c6efec27e1cca504b26
Gregory <gregory09ruiz@gmail.com>
2020-07-07 15:04:55 -0400

    Button_Nuevo

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,8 +5,14 @@
         <div class="col-md-12">
             <h1 class="page-header text-center">CRUD Laravel 6 con Autenticación</h1>
         </div>
-listado
+<br>
+<hr>
+<hr>
+<br>
         <div class="col-md-8">
+            <h2>Listado de Productos
+                <a href="{{ route('products.create') }}" class="btn btn-primary pull-right">Nuevo</a>
+            </h2>
 
         </div>
         <div class="col-md-4">
