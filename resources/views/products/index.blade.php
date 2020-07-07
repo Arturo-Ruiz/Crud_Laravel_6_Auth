de4f2aaf426ad1fb09dd93d76951ab5d546b2464
Gregory <gregory09ruiz@gmail.com>
2020-07-07 15:12:58 -0400

    table_products_and_paginate

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,15 +5,38 @@
         <div class="col-md-12">
             <h1 class="page-header text-center">CRUD Laravel 6 con Autenticación</h1>
         </div>
-<br>
-<hr>
-<hr>
-<br>
+            <br>
+            <hr>
+            <hr>
+            <br>
         <div class="col-md-8">
             <h2>Listado de Productos
                 <a href="{{ route('products.create') }}" class="btn btn-primary pull-right">Nuevo</a>
             </h2>
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th width="20px">ID</th>
+                        <th>Nombre del Producto</th>
+                        <th colspan="3">&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ( $products as $product )
+                    <tr>
+                        <td>{{$product->id}}</td>
+                        <td><strong>{{$product->name}}</strong>
+                            {{$product->short}}
+                        </td>
+                        <td>Ver</td>
+                        <td>Editar</td>
+                        <td>Borrar</td>
+                    </tr>
+                    @endforeach
+                </tbody>
 
+            </table>
+            {!! $products->render() !!}
         </div>
         <div class="col-md-4">
 
