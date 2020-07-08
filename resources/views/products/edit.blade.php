4889eae8a80fcd6a79e871c4d4d3024f81885620
Gregory <gregory09ruiz@gmail.com>
2020-07-07 21:39:12 -0400

    edit_ready

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -15,8 +15,9 @@
             <h2>Editar Producto
                 <a class="btn btn-primary pull-right" href="{{ route('products.index')}}">Listado</a>
             </h2>
-
-
+            {!! Form::model($product, ['route'=>['products.update', $product->id],'method'=>'PUT']) !!}
+            @include('products.fragment.form')
+            {!! Form::close() !!}
         </div>
         <div class="col-md-4">
             @include('products.fragment.aside')
